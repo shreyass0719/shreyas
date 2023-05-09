ad2341db9558ca1116d63fa55039f40ccdf1bc0d
shreyass0719 <133000603+shreyass0719@users.noreply.github.com>
2023-05-09 16:26:02 +0530

    Update sh1.java

--- sh1.java
+++ sh1.java
@@ -1,10 +1,10 @@
 public class abc{
-public static void main(String arg[])
-{
+   public static void main(String arg[])
+ { 
    int a=10;
    int b=20;
    int ans=a+b;
    System.out.println(ans);
-}
+  }
 } 
  
